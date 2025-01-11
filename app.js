b7088e0780cfa10bdd4853bac3e9ce776f64239b
MonieDa1st <128340611+22127021-22127235-22127279@users.noreply.github.com>
2025-01-11 22:46:05 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Chao thay Son!!!!');
+    res.send('Chao thay Son!');
 });
 
 const PORT = 5000;
